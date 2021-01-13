4658c6e36365f87dd751141228237b085cd0af19
Michael Mao <yt8956789.cs09g@nctu.edu.tw>
2021-01-13 13:35:58 +0000

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module myModule
+
+go 1.14
+
+require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.0
+	github.com/sirupsen/logrus v1.7.0
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package mymodule
+
+import (
+	formatter "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/sirupsen/logrus"
+	"os"
+	"time"
+)
+
+var log *logrus.Logger
+
+// AperLog : Log entry of aper
+var AperLog *logrus.Entry
+
+func init() {
+	log = logrus.New()
+	log.SetReportCaller(false)
+
+	log.Formatter = &formatter.Formatter{
+		TimestampFormat: time.RFC3339,
+		TrimMessages:    true,
+		NoFieldsSpace:   true,
+		HideKeys:        true,
+		FieldsOrder:     []string{"component", "category"},
+	}
+
+	AperLog = log.WithFields(logrus.Fields{"component": "MOD", "category": "TEST"})
+}
+
+func main() {
+	AperLog.Infof("Information !!")
+}
